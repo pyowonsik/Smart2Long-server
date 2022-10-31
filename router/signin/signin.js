d305801d6b2832b8b3afd210228eaefce5ba1fb1
표원식 <pyowonsik@pyowonsig-ui-MacBookAir.local>
2022-10-31 19:03:07 +0900

    modify all

--- router/signin/signin.js
+++ router/signin/signin.js
@@ -55,118 +55,82 @@ router.post("/",function (req,res){
 // 아이디 찾기
 router.get("/getemail/:name/:phone",(req,res) =>{
 
+
+
+
   const name = req.params.name;
   const phone = req.params.phone;
 
 
 
   const query = "select * from users where name = ? AND phone = ? ";
+    let data = {
+      "email " : "",
+    }
   connection.query(query,[name,phone],function(error,rows){
-
     console.log(rows[0]);
-    let data = rows[0].email;
+    data.email = rows[0].email;
     const json = JSON.stringify(data);
     jsonDatas = JSON.parse(json);
     let result = jsonDatas;
     console.log(result);
-    res.json(result);
-
-
+    return res.json(result);
   });
 });
 
 // 비밀번호 찾기
-router.get("/getpasswd/:email/:name/:phone",(req,res) =>{
+router.get("/getpasswd/:email/:name/",(req,res) =>{
 
   const email = req.params.email;
   const name = req.params.name;
-  const phone = req.params.phone;
 
-
-
-  const query = "select * from users where email = ? AND  name = ? AND phone = ? ";
-  connection.query(query,[email,name,phone],function(error,rows){
-
+  const query = "select * from users where email = ? AND  name = ? ";
+  connection.query(query,[email,name],function(error,rows){
+    let data = {
+      "passwd" : "",
+    }
     console.log(rows[0]);
-    let data = rows[0].passwd;
+    data.passwd = rows[0].passwd;
     const json = JSON.stringify(data);
     jsonDatas = JSON.parse(json);
     let result = jsonDatas;
     console.log(result);
-    res.json(result);
-
-
+    return res.json(result);
   });
 });
 
 
-// app.get("/item/:id", (req, res) =>{
-//   let id = req.params.id;
-//   let datas = fs.readFileSync("shoppingData.json");
-//   jsonDatas = JSON.parse(datas);
-//   console.log(jsonDatas);
-//   let result = jsonDatas.find((e) => e.id == id);
-//   return res.json(result);
+// // 회원삭제
+// router.post("/deleteusers",(req,res) =>{
+//   const passwd = req.params.passwd;
+//   const query = "delete from users where email = ?";
+//   connection.query(query,[email],function(error,rows){
+//     if(error){
+//       console.log(error);
+//        return res.status(400).json({ message:"delete failed"});
+//    }else{   
+//        return res.status(200).json({ message : "delete success"});
+//    }
+//   });
 // });
 
 
-// // sql ? 로 바꾸고  주석 플기
-// router.get("/begginer/:email",(req,res) => {
+// // 비밀번호 변경 
+// router.get("/chanepasswd",(req,res) =>{
 //   const email = req.params.email;
-//   const query = "select * from users where email = ? ";
-//   connection.query(query,[email], function(error,rows){
-//     // console.log(email);
-//     // console.log(rows[0]);
-//     // console.log(rows[0].begginer_re);
-//     if(error) console.log(error);
-//     let data = rows[0].begginer_re;
-//     jsonDatas = JSON.parse(data);
-//     console.log(jsonDatas);
-//     let result = jsonDatas;
-//     return res.json(result);
-//   })
+//   const passwd = req.params.passwd;
+//   const chagepasswd = req.params.chagepasswd;
+//   const query = "update users set passwd = 'changepasswd' where email = ?";
+//   connection.query(query,[email,passwd,chagepasswd],function(error,rows){
+//     if(error){
+//       console.log(error);
+//        return res.status(400).json({ message:"change failed"});
+//    }else{   
+//        return res.status(200).json({ message : "change success"});
+//    }
+//   });
 // });
 
 
-// router.get("/middler",(req,res) => {
-//   const email = req.body.email;
-//   const query = "select * from users ";
-//   connection.query(query,[email], function(error,rows){
-//     if(error) console.log(error);
-//     let data = rows[0].middler_re;
-//     jsonDatas = JSON.parse(data);
-//     console.log(jsonDatas);
-//     let result = jsonDatas;
-//     return res.json(result);
-//   })
-// });
-
-
-// router.get("/higher",(req,res) => {
-//   const email = req.body.email;
-//   const query = "select * from users ";
-//   connection.query(query,[email], function(error,rows){
-//     if(error) console.log(error);
-//     let data = rows[0].higher_re;
-//     jsonDatas = JSON.parse(data);
-//     console.log(jsonDatas);
-//     let result = jsonDatas;
-//     return res.json(result);
-//   })
-// });
-
-
-// router.get("/toiecer",(req,res) => {
-//   const email = req.body.email;
-//   const query = "select * from users ";
-//   connection.query(query,[email], function(error,rows){
-//     if(error) console.log(error);
-//     let data = rows[0].toiecer_re;
-//     jsonDatas = JSON.parse(data);
-//     console.log(jsonDatas);
-//     let result = jsonDatas;
-//     return res.json(result);
-//   })
-// });
 
 module.exports = router;
